7a42da2b5d308195f69b0a288942f0a41da21486
Oliver Marcusson <oliver.marcusson05@gmail.com>
2024-12-21 22:56:55 +0100

    .zshrc: added fastfetch, README: extended installation

---
+++ .omz-config/aliases.zsh
@@ -0,0 +1,9 @@
+alias install="sudo apt install -y"
+alias update="sudo apt update"
+alias upgrade="sudo apt upgrade"
+
+alias vpnc="cd ~/Ethical-Hacking/VPN && sudo ./vpn-connect.sh && cd"
+alias vpnd="cd ~/Ethical-Hacking/VPN && sudo ./vpn-connect.sh --disconnect && cd"
+alias python="python3"
+alias py="python3"
+alias ls="lsd"
