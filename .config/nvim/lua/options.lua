62035ad6f3b08a7eee4317f7aef6e78e6c9cc0c2
Oliver Marcusson <oliver.marcusson05@gmail.com>
2025-01-08 12:06:45 +0100

    Added jack support to java treesitter

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,5 +1,20 @@
 require "nvchad.options"
 
+vim.filetype.add({
+  extension = {
+    jack = "java",
+  },
+})
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "java" }, -- Ensure Java is installed
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  -- Add more configurations if needed
+}
+
 vim.opt.relativenumber = true
 
 -- add yours here!
